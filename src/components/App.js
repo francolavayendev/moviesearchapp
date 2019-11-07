8e7ac0fe5f5fe5cd34a405f80877631cabacb202
Franco Lavayen <francolavayendev@outlook.com>
2019-11-06 23:20:04 -0300

    fix typo

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Movie from './Movie';
